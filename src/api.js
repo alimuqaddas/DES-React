48c014e75930930bab5f002743a0b61d6c05459d
Ali Muqaddas <alimuqaddas92@gmail.com>
2017-09-18 23:57:44 +0500

    Done

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
     user:{ 
         login:(credentials) => //Passing credentials to this action
-            axios.post ('users/',{credentials}) // Making async api call
+            axios.post ('/users/',{credentials}) // Making async api call
             .then( // If everything is ok, recieving data as Res
                 res=>res.data.user)
     }
--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -42,111 +42,48 @@ class LoginForm extends Component{
 
 
         if(Object.keys(errors).length ===0){
-            this.setState({loading:true});
-            this.props.submit(this.state.data).catch(err => this.setState({errors:err.response.data.errors,loading:false}));
-        }
+          this.setState({loading:true});
+          this.props.submit(this.state.data).catch(err => this.setState({errors:err.response.data.errors,loading:false}));
+      }
     }
 
     render(){
         const {data , errors, loading}=this.state;
         return(
-            // <Form onSubmit={this.onSubmit} loading={loading}>
-            //     {errors.global && (
-            //         <Message negative>
-            //             <Message.Header>Reply from Server</Message.Header>
-            //             <p>{errors.global}</p>
-            //         </Message>
-            //     )}
-            //     <Form.Field>
-            //         <label htmlFor="email">Email</label>
-            //         <input
-            //             type="email"
-            //             id="email"
-            //             name="email"
-            //             placeholder="example@example.com"
-            //             value={data.email}
-            //             onChange={this.onChange}
-            //         />
-            //         {errors.email && <InLineError text={errors.email}/>}
-            //     </Form.Field>
-            //     <Form.Field>
-            //         <label htmlFor="password">Password</label>
-            //         <input
-                        // type="password"
-                        // id="password"
-                        // name="password"
-                        // placeholder="password"
-                        // onChange={this.onChange}
-            //         />
-            //         {errors.password && <InLineError text={errors.password}/>}
-            //     </Form.Field>
-            //     <Button primary>Login</Button>
-            // </Form>
-            <div className='login-form'>
-            {/*
-              Heads up! The styles below are necessary for the correct render of this example.
-              You can do same with CSS, the main idea is that all the elements up to the `Grid`
-              below must have a height of 100%.
-            */}
-            <style>{`
-              body > div,
-              body > div > div,
-              body > div > div > div.login-form {
-                height: 100%;
-              }
-            `}</style>
-            <Grid
-              textAlign='center'
-              style={{ height: '100%' }}
-              verticalAlign='middle'
-            >
-              <Grid.Column style={{ maxWidth: 450 }}>
-                <Header as='h2' color='teal' textAlign='center'>
-                  <Image src='/logo.png' />
-                  {' '}Log-in to your account
-                </Header>
-                <Form size='large' onSubmit={this.onSubmit} loading={loading}>
-                  <Segment stacked>
-                    <Form.Input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="example@example.com"
-                    value={data.email}
-                    onChange={this.onChange}
-                    fluid
-                    icon='user'
-                    iconPosition='left'
-                    placeholder='E-mail address'
-                    />
-                    {errors.email && <InLineError text={errors.email}/>}
-                    <Form.Input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="password"
-                    onChange={this.onChange}
-                      fluid
-                      icon='lock'
-                      iconPosition='left'
-                      placeholder='Password'
-                      type='password'
-                    />
-                    {errors.password && <InLineError text={errors.password}/>}
-                    <Button color='teal' fluid size='large' onClick={this.onSubmit}>Login</Button>
-                  </Segment>
-                </Form>
+          <div>
+            <Form onSubmit={this.onSubmit} loading={loading}>
                 {errors.global && (
                     <Message negative>
                         <Message.Header>Reply from Server</Message.Header>
                         <p>{errors.global}</p>
                     </Message>
-                 )}
-                <Message>
-                  New to us? <a href='#'>Sign Up</a>
-                </Message>
-              </Grid.Column>
-            </Grid>
+                )}
+                <Form.Field>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="example@example.com"
+                        value={data.email}
+                        onChange={this.onChange}
+                    />
+                    {errors.email && <InLineError text={errors.email}/>}
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="password"
+                        onChange={this.onChange}
+                    />
+                    {errors.password && <InLineError text={errors.password}/>}
+                </Form.Field>
+                <Button primary>Login</Button>
+            </Form>
+            
           </div>
         )
     }
